a706f3234408e9540d4a46f052f988aff7651f08
Eike Foken <e.foken@abian.de>
2016-01-26 17:06:12 +0100

    Linux support for GRC, .gitconfig example and a bin/ folder for tiny programs

--- grunt/completion.zsh
+++
@@ -1,3 +0,0 @@
-if (( $+commands[grunt] )); then
-    eval "$(grunt --completion=zsh)"
-fi
--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -17,6 +17,27 @@ alias sl='ls'
 alias vm='mv'
 
 # GRC colorizes nifty unix tools all over the place
-if (( $+commands[grc] )) && (( $+commands[brew] )); then
-    source `brew --prefix`/etc/grc.bashrc
+if (( $+commands[grc] )); then
+    if (( $+commands[brew] )); then
+        source `brew --prefix`/etc/grc.bashrc
+    else
+        alias grc='grc -es --colour=auto'
+        alias as='grc as'
+        alias df='grc df'
+        alias diff='grc diff'
+        alias dig='grc dig'
+        alias g++='grc g++'
+        alias gas='grc gas'
+        alias gcc='grc gcc'
+        alias head='grc head'
+        alias ld='grc ld'
+        alias make='grc make'
+        alias mount='grc mount'
+        alias mtr='grc mtr'
+        alias netstat='grc netstat'
+        alias ping='grc ping'
+        alias ps='grc ps'
+        alias tail='grc tail'
+        alias traceroute='grc traceroute'
+    fi
 fi
