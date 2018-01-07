67f8111966f4705b7f8928df1f12d6363aac8f7c
Eike Foken <kontakt@eikefoken.de>
2018-01-08 00:23:24 +0100

    Update Atom editor styles

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -1,3 +1,7 @@
 export RUBY_VERSION=`ruby -v | perl -ne '/ruby (\d+\.\d+\.\d+)/; print "$1";'`
+<<<<<<< Updated upstream
 export GOPATH=$HOME/.go
 export PATH=$ZSH/bin:$HOME/.gem/ruby/$RUBY_VERSION/bin:$GOPATH/bin:$PATH
+=======
+export PATH="$(brew --prefix homebrew/php/php70)/bin:$ZSH/bin:$HOME/.gem/ruby/$RUBY_VERSION/bin:$PATH"
+>>>>>>> Stashed changes
