e14cf4eacf6dd93034d82c32efff2cbaf66d433d
Eike Foken <eikefoken@gmail.com>
2019-02-19 11:50:31 +0100

    Update PATH variable

--- php/path.zsh
+++ php/path.zsh
@@ -1,2 +1,1 @@
-export PHP_VERSION=`cat Brewfile | grep 'homebrew\/php\/php\d\d' | grep -o 'php\d\d'`
-export PATH="$(brew --prefix homebrew/php/$PHP_VERSION)/bin:$PATH"
+export PATH="$HOME/.composer/vendor/bin:$PATH"
