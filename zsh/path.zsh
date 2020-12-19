751ddcf485549fe54dd7b1020aa34889dd445226
Eike Foken <eikefoken@gmail.com>
2020-12-19 20:10:14 +0100

    Update PATH for Android Studio

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -1,3 +1,4 @@
 export RUBY_VERSION=`ruby -v | perl -ne '/ruby (\d+\.\d+\.\d+)/; print "$1";'`
 export GOPATH=$HOME/.go
-export PATH=/usr/local/sbin:$ZSH/bin:$HOME/.gem/ruby/$RUBY_VERSION/bin:$GOPATH/bin:$PATH
+export ANDROID_HOME=$HOME/Library/Android/sdk
+export PATH=/usr/local/sbin:$ZSH/bin:$HOME/.gem/ruby/$RUBY_VERSION/bin:$GOPATH/bin:/usr/local/opt/curl/bin:/usr/local/opt/openssl/bin:$PATH:$ANDROID_HOME/emulator:$ANDROID_HOME/tools:$ANDROID_HOME/tools/bin:$ANDROID_HOME/platform-tools
